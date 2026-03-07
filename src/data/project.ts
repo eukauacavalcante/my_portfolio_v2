13e84ebd5219931441e019328d48029e317fbab1
Kauã Silva <kauasilvacavalcante21@gmail.com>
2026-03-07 03:25:06 -0300

    feat: add structure refactor and component enhancements
    - Refactor index page to use modular section components.
    - Create new section components (Blog, Services, Stack, Projects, Contact).
    - Enhance Button component with tailwind-merge and variants.
    - Create Card component with responsive design and tailwind-merge.

---
+++ src/data/project.ts
@@ -0,0 +1,34 @@
+const projects = [
+    {
+        image: "",
+        title: "Blog Pessoal",
+        phrase: "Compartilhando conhecimento e experiências no mundo da tecnologia",
+        resume: "Um blog pessoal para compartilhar meus pensamentos, tutoriais e experiências no mundo da tecnologia.",
+        description: "Um blog pessoal construído com Astro, Tailwind CSS e MDX para compartilhar meus pensamentos, tutoriais e experiências no mundo da tecnologia.",
+        tags: ["Astro", "Tailwind"],
+        slug: "blog-pessoal",
+        date: "2026",
+    },
+    {
+        image: "",
+        title: "Vortex Project",
+        phrase: "Rede social para entusiastas de tecnologia compartilharem projetos e ideias",
+        resume: "O Vortex Project é uma rede social voltada para entusiastas de tecnologia, onde os usuários podem compartilhar seus projetos, ideias e colaborações em um ambiente interativo e inspirador.",
+        description: "",
+        tags: ["Next.js", "Django", "PostgreSQL"],
+        slug: "vortex-project",
+        date: "2026",
+    },
+    {
+        image: "",
+        title: "Morpheus Env",
+        phrase: "Projeto acadêmico para monitoramento de desempenho em tempo e funções de conversão binária",
+        resume: "O Morpheu Env é um projeto acadêmico focado no monitoramento de desempenho em tempo real e na implementação de funções de conversão binária, visando otimizar processos e melhorar a eficiência em ambientes computacionais.",
+        description: "",
+        tags: ["Django", "Projeto Acadêmico"],
+        slug: "blog-pessoal",
+        date: "2025",
+    }
+];
+
+export default projects;
---
+++ src/data/stack.ts
@@ -0,0 +1,23 @@
+const stack = {
+    frontend: [
+        { name: "HTML5", icon: "/icons/react.svg" },
+        { name: "CSS3", icon: "/icons/vue.svg" },
+        { name: "JavaScript", icon: "/icons/svelte.svg" },
+        { name: "TypeScript", icon: "/icons/astro.svg" },
+        { name: "React", icon: "/icons/tailwind.svg" },
+        { name: "Tailwind", icon: "/icons/tailwind.svg" },
+        { name: "Next.js", icon: "/icons/tailwind.svg" },
+        { name: "Astro", icon: "/icons/tailwind.svg" },
+        { name: "Shadcn UI", icon: "/icons/tailwind.svg" },
+    ],
+    backend: [
+        { name: "Python", icon: "/icons/nodejs.svg" },
+        { name: "Django", icon: "/icons/nodejs.svg" },
+        { name: "Java", icon: "/icons/express.svg" },
+        { name: "Spring Boot", icon: "/icons/django.svg" },
+        { name: "MySQL", icon: "/icons/flask.svg" },
+        { name: "PostgreSQL", icon: "/icons/flask.svg" },
+    ]
+};
+
+export default stack;
