d7fe056c64d5c86e73fdfe1be66e5ed3b781fff6
Kauã Silva <kauasilvacavalcante21@gmail.com>
2026-04-19 22:09:51 -0300

    chore: remove React integration and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from '@tailwindcss/vite';
-import react from '@astrojs/react';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
@@ -11,5 +10,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [react(), sitemap()]
+  integrations: [sitemap()]
 });
